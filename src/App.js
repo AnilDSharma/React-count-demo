53d44fdd1faf6b14c3aa6f38ef6b67ddec128f70
AnilDSharma <anilsharma.udr56@gmail.com>
2023-04-14 17:54:06 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+ import Test from "./component/test"
+
+function App() {
+  let increment = 0;
+  let decrement = 0;
+  return (
+    <Test />
+    // <form>
+    //   {/* <label>
+    //     First Name:
+    //     <input type="text" name="fname" />
+    //   </label>
+    //   <br />
+    //   <label>
+    //     Last Name:
+    //     <input type="text" name="lname" />
+    //   </label>
+    //   <br />
+    //   <label>
+    //     Email:
+    //     <input type="text" name="email" />
+    //   </label>
+    //   <br />
+    //   <input type="submit" value="Submit" /> */}
+      
+    // </form>
+  );
+}
+
+export default App;
